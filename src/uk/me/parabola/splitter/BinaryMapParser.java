ef630756764f3ea2fd50edaef8d5e678e47e3cd0
steve <steve@838fabe5-b1a8-4b81-9896-7a84386980e9>
2011-09-25 11:27:16 +0000

    Do not add a bounding box if there is not one in the input. Applies to PBF input files.
    git-svn-id: http://svn.mkgmap.org.uk/splitter/trunk@180 838fabe5-b1a8-4b81-9896-7a84386980e9

--- src/uk/me/parabola/splitter/BinaryMapParser.java
+++ src/uk/me/parabola/splitter/BinaryMapParser.java
@@ -1,10 +1,9 @@
 package uk.me.parabola.splitter;
 
-import java.util.List;
-
-
 import crosby.binary.BinaryParser;
 import crosby.binary.Osmformat;
+
+import java.util.List;
 
 public class BinaryMapParser extends BinaryParser {
 	// How many elements to process before displaying a status update
@@ -32,12 +31,12 @@ public class BinaryMapParser extends BinaryParser {
 		long last_id = 0, last_lat = 0, last_lon = 0;
 		int j = 0; 
 		for (int i=0 ; i < nodes.getIdCount(); i++) {
-			Node tmp = new Node();
 			long lat = nodes.getLat(i)+last_lat; last_lat = lat;
 			long lon = nodes.getLon(i)+last_lon; last_lon = lon;
 			long id =  nodes.getId(i)+last_id; last_id = id;
             double latf = parseLat(lat), lonf = parseLon(lon);
-			tmp = new Node();
+			
+			Node tmp = new Node();
 			tmp.set((int)id, latf, lonf);
             if (nodes.getKeysValsCount() > 0) {
                 while (nodes.getKeysVals(j) != 0) {
@@ -87,6 +86,7 @@ public class BinaryMapParser extends BinaryParser {
 			processWays(tmp);
 		}
 	}
+
 	protected void parseRelations(List<Osmformat.Relation> rels) {
 		for (Osmformat.Relation i : rels) {
 			Relation tmp = new Relation();
@@ -110,7 +110,7 @@ public class BinaryMapParser extends BinaryParser {
 				else if (i.getTypes(j) == Osmformat.Relation.MemberType.RELATION)
 					continue;
 				else
-						assert false; // TODO; Illegal file?
+					assert false; // TODO; Illegal file?
 
 				tmp.addMember(etype,(int)mid,role);
 			}
@@ -120,28 +120,30 @@ public class BinaryMapParser extends BinaryParser {
 	}
 
 	public void parse(Osmformat.HeaderBlock block) {
-		double multiplier = .000000001;
-		double rightf = block.getBbox().getRight() * multiplier;
-		double leftf = block.getBbox().getLeft() * multiplier;
-		double topf = block.getBbox().getTop() * multiplier;
-		double bottomf = block.getBbox().getBottom() * multiplier;
 
 		for (String s : block.getRequiredFeaturesList()) {
             if (s.equals("OsmSchema-V0.6")) continue; // OK.
             if (s.equals("DenseNodes")) continue; // OK.
-            throw new Error("File requires unknown feature: " + s);
+            throw new UnknownFeatureException(s);
         }
 
-		System.out.println("Bounding box "+leftf+" "+bottomf+" "+rightf+" "+topf);
-		
-		Area area = new Area(
-				Utils.toMapUnit(bottomf),
-				Utils.toMapUnit(leftf),
-				Utils.toMapUnit(topf),
-				Utils.toMapUnit(rightf));
-		processor.boundTag(area);
+		if (block.hasBbox()) {
+			final double multiplier = .000000001;
+			double rightf = block.getBbox().getRight() * multiplier;
+			double leftf = block.getBbox().getLeft() * multiplier;
+			double topf = block.getBbox().getTop() * multiplier;
+			double bottomf = block.getBbox().getBottom() * multiplier;
+
+			System.out.println("Bounding box "+leftf+" "+bottomf+" "+rightf+" "+topf);
+
+			Area area = new Area(
+					Utils.toMapUnit(bottomf),
+					Utils.toMapUnit(leftf),
+					Utils.toMapUnit(topf),
+					Utils.toMapUnit(rightf));
+			processor.boundTag(area);
+		}
 	}
-	
 
 	private void processNodes(Node tmp) {
 		nodeCount++;
@@ -149,19 +151,20 @@ public class BinaryMapParser extends BinaryParser {
 			System.out.println(Utils.format(nodeCount) + " nodes processed... id="+tmp.getId());
 		}
 
-}
+	}
 
-private void processWays(Way tmp)  {
-	wayCount++;
-	if (wayCount % WAY_STATUS_UPDATE_THRESHOLD == 0) {
-		System.out.println(Utils.format(wayCount) + " ways processed... id="+tmp.getId());
+	private void processWays(Way tmp)  {
+		wayCount++;
+		if (wayCount % WAY_STATUS_UPDATE_THRESHOLD == 0) {
+			System.out.println(Utils.format(wayCount) + " ways processed... id="+tmp.getId());
+		}
 	}
-}
-private void processRelations(Relation tmp)  {
-	relationCount++;
-	if (relationCount % RELATION_STATUS_UPDATE_THRESHOLD == 0) {
-		System.out.println(Utils.format(relationCount) + " relations processed... id="+tmp.getId());
+
+	private void processRelations(Relation tmp)  {
+		relationCount++;
+		if (relationCount % RELATION_STATUS_UPDATE_THRESHOLD == 0) {
+			System.out.println(Utils.format(relationCount) + " relations processed... id="+tmp.getId());
+		}
 	}
-}
 
 }
