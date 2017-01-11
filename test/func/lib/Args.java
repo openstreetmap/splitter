ab20060cb8a9b0dd5bff351b5b6a9825398ee26b
gerd <gerd@838fabe5-b1a8-4b81-9896-7a84386980e9>
2017-01-11 09:16:57 +0000

    add framework for functional tests (copied from mkgmap) add some basic functional tests
    git-svn-id: http://svn.mkgmap.org.uk/splitter/branches/junit@563 838fabe5-b1a8-4b81-9896-7a84386980e9

---
+++ test/func/lib/Args.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Gerd Petermann
+ * Create date: 2017-01-10
+ */
+package func.lib;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Useful constants that are used for arguments etc. in the functional
+ * tests.
+ *
+ * @author Gerd Petermann
+ */
+public interface Args {
+	public static final String TEST_RESOURCE_OSM = "test/resources/in/osm/";
+
+	public static final String DEF_TEMPLATE = "template.args";
+	public static final String DEF_DENSITIES = "densities-out.txt";
+	public static final String DEF_AREAS_KML = "areas.kml";
+	public static final String DEF_AREAS_LIST = "areas.list";
+	public static final String DEF_AREAS_POLY = "areas.poly";
+	public static final String DEF_PROBLEM_LIST = "problem.list";
+	
+	public static final String[] MAIN_ARGS = { "--status-freq=0", 
+			"--write-kml=" + DEF_AREAS_KML,
+			"--problem-report=" + DEF_PROBLEM_LIST,
+			"--max-nodes=500000",
+			};
+	
+	public static final String ALASKA = TEST_RESOURCE_OSM + "alaska-2016-12-27.osm.pbf";
+	public static final String HAMBURG = TEST_RESOURCE_OSM + "hamburg-2016-12-26.osm.pbf";
+
+	/** expected file sizes for ALASKA file */
+	public static final Map<String, Integer> expectedAlaska = new LinkedHashMap<String, Integer>() {
+		{
+			put(DEF_AREAS_KML, 5373);
+			put(DEF_AREAS_LIST, 1142);
+			put(DEF_AREAS_POLY, 484);
+			put(DEF_DENSITIES, 840190);
+			put(DEF_PROBLEM_LIST, 13727);
+			put(DEF_TEMPLATE, 1034);
+		}
+	};
+
+	/** expected file sizes for HAMBURG file */
+	public static final Map<String, Integer> expectedHamburg = new LinkedHashMap<String, Integer>() {
+		{
+			put(DEF_AREAS_KML, 3278);
+			put(DEF_AREAS_LIST, 658);
+			put(DEF_AREAS_POLY, 289);
+			put(DEF_DENSITIES, 2317);
+			put(DEF_PROBLEM_LIST, 57647);
+			put(DEF_TEMPLATE, 734);
+		}
+	};
+}
